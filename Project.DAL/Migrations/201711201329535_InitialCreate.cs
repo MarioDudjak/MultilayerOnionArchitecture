a978227976fd58aaeb16623e67c96a63e50f3f84
MarioDudjak <dudjakmario@hotmail.com>
2017-11-20 18:41:16 +0100

    Enabled and added migrations

---
+++ Project.DAL/Migrations/201711201329535_InitialCreate.cs
@@ -0,0 +1,27 @@
+namespace Project.DAL.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Guid(nullable: false, identity: true),
+                        Username = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Password = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
